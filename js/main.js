cc060a5395b5aa6f4cf2881a5ec3faa3c3a2ba5d
JJ Guaragno <just2jays@gmail.com>
2017-11-21 12:50:03 -0500

    this is v0.0.2 - weve added a new cool cat soldier to the mix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-    $('body').append('<div id="mainbox" class="plain-box"><img src="'+chrome.extension.getURL('images/free_walker.gif')+'" /></div>');
+    $('body').append('<div id="mainbox" class="plain-box"><div class="sprite cat-soldier"></div></div>');
     beginJourney();
+
+    $('#mainElement').on('click', '.new-character', newCharacter);
 });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        //console.log(request);
+        newCharacter(request.name);
+    }
+);
 
 function beginJourney() {
     var $walker = $('.plain-box');
@@ -29,4 +38,15 @@ function beginJourney() {
         $walker.stop();
         $walker.animate({ top: event.pageY, left: event.pageX }, 10000);
     });
+}
+
+function newCharacter(name) {
+    $('#mainbox').remove();
+    if( name == "soldier" ) {
+        $('body').append('<div id="mainbox" class="plain-box"><div class="sprite cat-soldier"></div></div>');
+        beginJourney();
+    }else{
+        $('body').append('<div id="mainbox" class="plain-box"><img src="'+chrome.extension.getURL('images/free_walker.gif')+'" /></div>');
+        beginJourney();
+    }
 }
