f015fc6d62feea3f267f1cddf1bebd64d5a6d956
JJ <just2jays@gmail.com>
2017-12-02 18:17:37 -0500

    [update] add the Puppy

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -26,13 +26,26 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Drunk Rabbit
+    // THE ROBOT
     document.getElementById("CharacterButtonC").onclick = function(){
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             if(tabs.length == 0){ 
                 console.log("could not send mesage to current tab");
             }else{
                 chrome.tabs.sendMessage(tabs[0].id, {name: "the-robot"}, function(response) {
+                    // console.log(response);
+                });
+            }
+        });
+    }
+
+    // PUPPY
+    document.getElementById("CharacterButtonD").onclick = function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            if(tabs.length == 0){ 
+                console.log("could not send mesage to current tab");
+            }else{
+                chrome.tabs.sendMessage(tabs[0].id, {name: "puppy"}, function(response) {
                     // console.log(response);
                 });
             }
